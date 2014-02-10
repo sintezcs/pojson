24dfa223ca8ed35010b921cb825552e5b9259674
Alexey Minakov <sintez.pm@gmail.com>
2014-02-11 00:18:56 +0400

    Revert c84e11d..5dac4f1
    This rolls back to commit c84e11d022f481549cc07e09607cf7bfb39252e6.

--- pojson/main.py
+++ pojson/main.py
@@ -1,21 +1,19 @@
-import argparse
+import argh
 import pojson
-import os
+
+@argh.arg('-p', '--pretty-print', default=False, help="Pretty-print JSON")
+@argh.arg('-e', '--encoding', default=None, help="Encoding of PO file")
+@argh.arg('-j', '--javascript', default=False, help="Generate JS instead of JSON")
+@argh.arg('domain', help='Translation domain')
+@argh.arg('po_file', help='PO file')
+def convert(args):
+    print pojson.convert(args.domain,
+                         args.po_file,
+                         js=args.javascript,
+                         encoding=args.encoding,
+                         pretty_print=args.pretty_print).encode('utf-8')
 
 def main():
-    p = argparse.ArgumentParser(description='convert .po to .json')
-    p.add_argument('po_file')
-    p.add_argument('-e', '--encoding', help='encoding of po file')
-    p.add_argument('-p', '--pretty-print', help='pretty-print JSON output',
-                   action="store_true")
-    args = p.parse_args()
-
-    if not os.path.isfile(args.po_file):
-        p.exit(u"not a file: %s" % args.po_file)
-    if not args.po_file.endswith('.po'):
-        p.exit(u"not a PO file: %s" % args.po_file)
-
-    print pojson.convert(
-        args.po_file, encoding=args.encoding,
-        pretty_print=args.pretty_print).encode('utf-8')
-    
+    p = argh.ArghParser()
+    p.add_commands([convert])
+    p.dispatch()
